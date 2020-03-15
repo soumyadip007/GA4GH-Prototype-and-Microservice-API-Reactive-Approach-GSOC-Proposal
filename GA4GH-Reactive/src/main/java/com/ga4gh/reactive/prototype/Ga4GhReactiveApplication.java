a072dea990d24a71bceb2124dc4d1b425a5f6801
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 22:56:50 +0530

    GA4GH Prototype Reactive Approach Initial Commit

---
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/Ga4GhReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.ga4gh.reactive.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga4GhReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ga4GhReactiveApplication.class, args);
+	}
+
+}
---
+++ GA4GH-Reactive/src/test/java/com/ga4gh/reactive/prototype/GA4GH/Reactive/Ga4GhReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga4gh.reactive.prototype.GA4GH.Reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ga4GhReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
