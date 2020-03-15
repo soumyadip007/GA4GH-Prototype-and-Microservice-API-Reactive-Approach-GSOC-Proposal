09360df1f07c8c6c243aba637c26265c533d9d27
soumyadip007 <soumyadip.note@gmail.com>
2020-03-16 01:17:05 +0530

    ga4ghID Mono added

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/controller/ReactiveController.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/controller/ReactiveController.java
@@ -1,6 +1,7 @@
 package com.ga4gh.reactive.prototype.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import com.ga4gh.reactive.prototype.entity.GA4GH;
 import com.ga4gh.reactive.prototype.repository.GA4GHRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/ga4gh")
@@ -19,6 +21,12 @@ public class ReactiveController {
 	public Flux<GA4GH> ga4ghFlux()
 	{
 		return this.GA4GHRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Mono<GA4GH> ga4ghID(@PathVariable final int i)
+	{
+		return this.GA4GHRepository.findById(i);
 	}
 	
 }
