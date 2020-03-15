64ae72232a9cea5b16254fed399968f643dd27b0
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 23:28:04 +0530

    Lombok properties added

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/entity/GA4GH.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/entity/GA4GH.java
@@ -4,12 +4,18 @@ package com.ga4gh.reactive.prototype.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Getter	
 @Setter	
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class GA4GH {
 
