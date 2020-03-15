80a8980feeb048b97eb2c68babfed877aedaf6f1
soumyadip007 <soumyadip.note@gmail.com>
2020-03-16 01:13:54 +0530

    Add Data In Command Line

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/Ga4GhReactiveApplication.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/Ga4GhReactiveApplication.java
@@ -1,11 +1,39 @@
 package com.ga4gh.reactive.prototype;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ga4gh.reactive.prototype.entity.GA4GH;
+import com.ga4gh.reactive.prototype.repository.GA4GHRepository;
 
 @SpringBootApplication
 public class Ga4GhReactiveApplication {
 
+	
+	@Bean
+	CommandLineRunner addChromo(GA4GHRepository ga4ghRepository){
+		return args -> {
+			ga4ghRepository
+					.deleteAll()
+			.subscribe(null, null, () -> {
+
+				Stream.of(new GA4GH(1,"A","A","A","A" )
+						)
+						.forEach(ga4gh -> {
+					ga4ghRepository
+						.save(ga4gh)
+						.subscribe(System.out::println);
+						});
+
+			})
+			;
+		};	
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Ga4GhReactiveApplication.class, args);
 	}
