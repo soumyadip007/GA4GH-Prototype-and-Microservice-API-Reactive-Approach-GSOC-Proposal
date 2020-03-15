59cdfcc4526cb4a676c8e1e1798470f3b4d7653f
soumyadip007 <soumyadip.note@gmail.com>
2020-03-15 23:31:15 +0530

    GA4GHRepository.java Mongo Added

---
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/mongo/repository/GA4GHRepository.java
@@ -0,0 +1,11 @@
+package com.ga4gh.reactive.prototype.mongo.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ga4gh.reactive.prototype.entity.GA4GH;
+
+//@Repository("GA4GHRepository")
+public interface GA4GHRepository extends ReactiveMongoRepository<GA4GH, Integer> {
+
+}
