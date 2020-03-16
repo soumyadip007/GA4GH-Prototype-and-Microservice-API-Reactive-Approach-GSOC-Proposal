0b3f4af38f81860f84632999072e57288fa59dcb
soumyadip007 <soumyadip.note@gmail.com>
2020-03-16 23:04:43 +0530

    Find All Server Response

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/handler/RouterHandler.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/handler/RouterHandler.java
@@ -1,33 +1,28 @@
 package com.ga4gh.reactive.prototype.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import reactor.core.publisher.Flux;
+import com.ga4gh.reactive.prototype.entity.GA4GH;
+import com.ga4gh.reactive.prototype.repository.GA4GHRepository;
+
 import reactor.core.publisher.Mono;
 
 @Component
 public class RouterHandler {
 	
+	@Autowired
+	private GA4GHRepository GA4GHRepository;
+	
 	public Mono<ServerResponse> getAll(ServerRequest serverRequest){
 			
 			return ServerResponse.ok()
-					.contentType(MediaType.APPLICATION_JSON)
-					.body(
-					Flux.just("GA4GH"," genbankaccn"," refseqaccn",
-						" sequencelength").log(), String.class);
+					.body(GA4GHRepository.findAll(),GA4GH.class);
 					
 		}
-	
-	public Mono<ServerResponse> mono(ServerRequest serverRequest){
-		
-		return ServerResponse.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(
-				Mono.just("GA4GH").log(), String.class);
-				
-	}
+
 
 }
