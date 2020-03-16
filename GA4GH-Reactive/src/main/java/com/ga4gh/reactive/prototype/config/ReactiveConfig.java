249112820687b25127f5127ed93d67087f03f93b
soumyadip007 <soumyadip.note@gmail.com>
2020-03-16 22:57:50 +0530

    Reactive Config

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/config/ReactiveConfig.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/config/ReactiveConfig.java
@@ -2,15 +2,23 @@ package com.ga4gh.reactive.prototype.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
 
 @Configuration
 public class ReactiveConfig {
 
 	@Bean
-	RouterFunction<?> routerFunction(){
+	RouterFunction<?> routerFunction(Route){
 		
-		
+		RouterFunctions.route(RequestPredicate.GET("/ga4gh/flux")
+				
+				)
 		
 	}
 }
