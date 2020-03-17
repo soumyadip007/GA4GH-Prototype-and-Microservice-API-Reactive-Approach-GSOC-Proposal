7210afcba7c903b06ca5b66d9fc3f1160e6ddbf2
soumyadip007 <soumyadip.note@gmail.com>
2020-03-17 13:09:31 +0530

    Reactive Config Post Request access

--- GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/config/ReactiveConfig.java
+++ GA4GH-Reactive/src/main/java/com/ga4gh/reactive/prototype/config/ReactiveConfig.java
@@ -1,23 +1,46 @@
 package com.ga4gh.reactive.prototype.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.BodyExtractor;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 
+import com.ga4gh.reactive.prototype.entity.GA4GH;
 import com.ga4gh.reactive.prototype.handler.RouterHandler;
+import com.ga4gh.reactive.prototype.repository.GA4GHRepository;
+
+import reactor.core.publisher.Mono;
 
 
 @Configuration
 public class ReactiveConfig {
 
+
+	@Autowired
+	private GA4GHRepository GA4GHRepository;
 	@Bean
 	RouterFunction<?> routerFunction(RouterHandler routehandler){
 	     return RouterFunctions
 	   .route(RequestPredicates.GET("/rest/ga4gh/all"), routehandler::getAll)
 	                .andRoute(RequestPredicates.GET("/rest/ga4gh/{id}"), routehandler::get)
-				;
+//	                .andRoute(RequestPredicates.POST("/person"),
+//	                	    request -> {
+//	                	        Mono<GA4GH> OBJ= (Mono<GA4GH>) request.body(toMono(GA4GH.class));
+//	                	        return ResponseEntity.ok().build(GA4GHRepository.save(OBJ));
+//	                	      })
+	                ;
 		
 	}
+//	private BodyExtractor toMono(Class<GA4GH> class1) {
+//		// TODO Auto-generated method stub
+//		
+//		Class<GA4GH> obj=class1;
+//		return class1;
+//	}
+	
 }
